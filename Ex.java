11649fc611cc42064ecdc758fb28f37b20cf53dc
22A81A43C9 <satishveeravalli2004@gmail.com>
2026-06-27 23:10:03 +0530

    update

--- Ex.java
+++ Ex.java
@@ -14,7 +14,7 @@ public class Ex
                 arr[i]=sc.nextInt();
             }
             System.out.println(Arrays.toString(arr));
-            System.out.println("hello world");
+            System.out.println("helloworld");
             System.out.println(arr[n+1]);
 
 

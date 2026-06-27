cfc723a5bb38fa08b59ac498f4157687efef3b19
22A81A43C9 <satishveeravalli2004@gmail.com>
2026-06-27 22:59:45 +0530

    Exception program

---
+++ Ex.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Ex
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        try{
+            System.out.println("Enter Array size: ");
+            int n=sc.nextInt();
+            int[] arr=new int[n];
+            System.out.println("Enter Array Elements: ");
+            for(int i=0;i<n;i++)
+            {
+                arr[i]=sc.nextInt();
+            }
+            System.out.println(Arrays.toString(arr));
+            System.out.println(arr[n+1]);
+
+
+        }
+
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Array Index Out Of bound Exception");
+        }
+    }
+}
